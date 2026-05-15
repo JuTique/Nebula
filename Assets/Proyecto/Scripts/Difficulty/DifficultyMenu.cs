b2b0f9de77ecaf677d5fd2eeb7fc5dc91e03afc6
Stella <steluc2005@gmail.com>
2026-05-14 23:20:21 -0500

    Nivel visualmente con  sprites propios

--- Assets/Proyecto/Scripts/Difficulty/DifficultyMenu.cs
+++ Assets/Proyecto/Scripts/Difficulty/DifficultyMenu.cs
@@ -2,17 +2,43 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using MoreMountains.CorgiEngine;
 using MoreMountains.Tools;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class DifficultyMenu : MonoBehaviour
 {
     [SerializeField] private GameObject difficultyMenuPanel;
+    [SerializeField] private Button[] difficultyButtons;
 
     public void SelectDifficulty(int difficulty)
     {
         DifficultyManager.SetDifficulty((DifficultyManager.Difficulty)difficulty);
-        
-        Debug.Log("Dificultad cambiada. Ocultando menú...");
-        HideDifficultyMenu();
+        Debug.Log("Dificultad cambiada: " + difficulty);
+
+        // Mantener el panel visible y marcar el botón seleccionado.
+        if (difficultyButtons != null && difficulty >= 0 && difficulty < difficultyButtons.Length)
+        {
+            // Reactivar todos los botones primero
+            for (int i = 0; i < difficultyButtons.Length; i++)
+            {
+                if (difficultyButtons[i] != null)
+                {
+                    difficultyButtons[i].interactable = true;
+                }
+            }
+
+            // "Dejar presionado" el botón seleccionado desactivando su interactable
+            var btn = difficultyButtons[difficulty];
+            if (btn != null)
+            {
+                btn.interactable = false;
+                // también lo seleccionamos en el EventSystem para foco visual
+                if (EventSystem.current != null)
+                {
+                    EventSystem.current.SetSelectedGameObject(btn.gameObject);
+                }
+            }
+        }
     }
 
     public void ShowDifficultyMenu()
